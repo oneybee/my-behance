a852c1f0ce28164ee938ef138e06deb87ef017d8
B <hojong6636@gmail.com>
2019-04-30 01:32:12 +0900

    load seoul weather API data

--- src/Upload/index.js
+++ src/Upload/index.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
 
-var openWeatherMapKey = '59bc39177608b635b3514f8156021c3f'
-
-var obj = {
-  method: 'GET',
-  headers: {
-    'X-Auth-Token': 'e600784123014d489c796ab72ed9587d',
-  },
-}
+// const data = [{ name: 'name', value: 'seoul' }, { name: 'cnt', value: 39 }]
 
 class weather extends Component {
   constructor() {
-    console.log('name')
     super()
     this.state = {
-      data: [],
+      data: [{ name: 'name', value: 'seouls' }, { name: 'cnt', value: 19 }],
     }
   }
   componentDidMount() {
-    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=59bc39177608b635b3514f8156021c3f')
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=seoul&APPID=59bc39177608b635b3514f8156021c3f')
       .then(response => response.json())
       .then(responseJson => {
-        // console.log('weather')
         console.log(responseJson)
-
+        // console.log(responseJson.cnt)
         this.setState({
-          data: [{ name: 'London' }],
+          data: [{ name: responseJson.city.name, cnt: responseJson.cnt, cloud: responseJson.list[2].clouds.all }],
         })
-        this.setState()
+        this.setState({})
       })
   }
   render() {
     return (
       <div>
-        <span>weather api</span>
+        <div>weather api</div>
+        <br />
+        city name={this.state.data[0]['name']}
+        <br />
+        seoul cnt={this.state.data[0]['cnt']}
+        <br />
+        seoul cloud info={this.state.data[0]['cloud']}
       </div>
     )
   }
