10aa1b62385e0d89d6f0420224e616fc750eced3
B <hojong6636@gmail.com>
2019-04-30 21:02:46 +0900

    finally load nextprops

--- src/Upload/index.js
+++ src/Upload/index.js
@@ -5,6 +5,10 @@ import axios from 'axios'
 var lat = ['35.555861199999995']
 var lon = ['127.0780834']
 
+export const sleep = sec => {
+  return new Promise(resolve => setTimeout(resolve, sec * 1000))
+}
+
 //geolocated props example code
 class Demo extends React.Component {
   render() {
@@ -42,11 +46,17 @@ class Demo extends React.Component {
     )
   }
 }
+geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(Demo)
 
 //upload page
 class Upload extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       data: [{ name: 'name', value: 'ssss' }, { name: 'cnt', value: 19 }],
       lat: [],
@@ -54,7 +64,7 @@ class Upload extends React.Component {
     }
   }
 
-  componentDidMount() {
+  async componentDidMount(props) {
     //fetch weather api
 
     // fetch('http://api.openweathermap.org/data/2.5/forecast?APPID=59bc39177608b635b3514f8156021c3f&lat=' + '35' + '&lon=' + lon)
@@ -64,37 +74,79 @@ class Upload extends React.Component {
     //     lon: 127,
     //   },
     // })
+    console.log('test1', this.props)
+
+    // await sleep(2)
+
+    console.log('test3', this.props)
 
     //axios code and it works well, but state was not work well
+    console.log('test')
+    await sleep(2)
+
+    // console.log(this.props.coords.latitude)
+    // axios
+    //   .get('http://api.openweathermap.org/data/2.5/weather', {
+    //     params: {
+    //       lat: 35,
+    //       lon: 30.87,
+    //       appid: '59bc39177608b635b3514f8156021c3f',
+    //     },
+    //   })
+    //   .then(responseJson => {
+    //     // console.log(responseJson.data.name)
+    //     // console.log(this.props.coords.longitude)
+    //     console.log('test2', this.props)
+
+    //     this.setState({
+    //       // data: [{ name: responseJson.city.name, cnt: responseJson.cnt, cloud: responseJson.list[2].clouds.all }],
+    //       data: [{ name: responseJson.data.name }],
+    //       // lat: [this.props.coords.latitude],
+    //       // lon: this.props.coords.longitude,
+    //     })
+    //     // console.log(lat)
+    //     // console.log(lon)
+    //   })
+
+    // .then(response => response.json())
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log('nextProps', nextProps)
+    console.log('sss', nextProps.coords.latitude)
 
     axios
       .get('http://api.openweathermap.org/data/2.5/weather', {
         params: {
-          lat: 30.18,
-          lon: 30.87,
+          lat: nextProps.coords.latitude,
+          lon: nextProps.coords.longitude,
           appid: '59bc39177608b635b3514f8156021c3f',
         },
       })
-
-      // .then(response => response.json())
       .then(responseJson => {
-        console.log(responseJson.data.name)
-        console.log(this.props.coords.latitude)
-        console.log(this.props.coords.longitude)
+        // console.log(responseJson.data.name)
+        // console.log(this.props.coords.longitude)
+        console.log('test2', this.props)
 
         this.setState({
           // data: [{ name: responseJson.city.name, cnt: responseJson.cnt, cloud: responseJson.list[2].clouds.all }],
           data: [{ name: responseJson.data.name }],
+          // lat: [this.props.coords.latitude],
+          // lon: this.props.coords.longitude,
         })
+        // console.log(lat)
+        // console.log(lon)
       })
+    // this.setState({
+    //   likesIncreasing: nextProps.likeCount > this.props.likeCount
+    // });
   }
   render() {
     return (
       <div>
+        {/* <Demo /> */}
         <div>weather api</div>
         <br />
         city name={this.state.data[0]['name']}
-        {/* {responseJson.data.name} */}
         <br />
         seoul cnt={this.state.data[0]['cnt']}
         <br />
@@ -102,6 +154,38 @@ class Upload extends React.Component {
         <br />
       </div>
     )
+
+    // return (
+    //   <div>
+    //     {/* <div> city name={this.state.data[0]['name']}</div> */}
+    //     !this.props.isGeolocationAvailable ? (<div>Your browser does not support Geolocation</div>) : !this.props.isGeolocationEnabled ? (<div>Geolocation is not enabled</div>) : this.props.coords ? (
+    //     <table>
+    //       <tbody>
+    //         <tr>
+    //           <td>latitude</td>
+    //           <td>{this.props.coords.latitude}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>longitude</td>
+    //           <td>{this.props.coords.longitude}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>altitude</td>
+    //           <td>{this.props.coords.altitude}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>heading</td>
+    //           <td>{this.props.coords.heading}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>speed</td>
+    //           <td>{this.props.coords.speed}</td>
+    //         </tr>
+    //       </tbody>
+    //     </table>
+    //     ) : (<div>Getting the location data&hellip; </div>)
+    //   </div>
+    // )
   }
 }
 
