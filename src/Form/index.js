6151ae261ced64a1b44a31894aca25f79ce63070
B <hojong6636@gmail.com>
2019-05-05 18:17:49 +0900

    add invaild message on email

--- src/Form/index.js
+++ src/Form/index.js
@@ -22,23 +22,13 @@ const Image = styled.img`
 
 const validate = values => {
   const errors = {}
-  if (!values.username) {
-    errors.username = 'Required'
-  } else if (values.username.length > 15) {
-    errors.username = 'Must be 15 characters or less'
-  }
+
   if (!values.email) {
     errors.email = 'Required'
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address'
   }
-  if (!values.age) {
-    errors.age = 'Required'
-  } else if (isNaN(Number(values.age))) {
-    errors.age = 'Must be a number'
-  } else if (Number(values.age) < 18) {
-    errors.age = 'Sorry, you must be at least 18 years old'
-  }
+
   return errors
 }
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
@@ -46,17 +36,11 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
     <label>{label}</label>
     <div>
       <input {...input} placeholder={label} type={type} />
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      {touched && (error && <span>{error}</span>)}
     </div>
   </div>
 )
-const warn = values => {
-  const warnings = {}
-  if (values.age < 19) {
-    warnings.age = 'Hmm, you seem a bit young...'
-  }
-  return warnings
-}
+
 const Form = props => {
   const { handleSubmit, pristine, reset, submitting } = props
 
@@ -108,5 +92,4 @@ const Form = props => {
 export default reduxForm({
   form: 'payment', // a unique identifier for this form
   validate, // <--- validation function given to redux-form
-  warn, //
 })(Form)
